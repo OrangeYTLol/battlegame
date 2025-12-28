ab47e74e2a6efce2a2aff402ec6394674446f81f
OrangeYTLol <alexanderjcantrell@gmail.com>
2025-12-27 20:18:37 -0500

    Created tile class

--- map/tile.py
+++ map/tile.py
@@ -1,2 +1,24 @@
+from entities.spriteProvider import SpriteProvider
+
 class Tile:
-    pass
+    def __init__(self, tileID):
+        self.tileset = tileID[:2]
+        self.tileNum = int(tileID[3])
+        self.layer = tileID[5]
+    
+    def getSprites(self):
+        
+        sp = SpriteProvider()
+        tiles = {
+            "b1": {
+                1: ("tiles/tileset", [(0, 0, 16, 16)])
+                }
+        }
+        tile = tiles[self.tileset][self.tileNum][1]
+        sheet = tiles[self.tileset][self.tileNum][0]
+        
+        sp.loadSheet(sheet)
+        return sp.getSprites(tile)
+        
+        
+        
